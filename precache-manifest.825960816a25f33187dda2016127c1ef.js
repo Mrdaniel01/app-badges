5f4a7c6947caf4d91ea75605b421d2e75f7176ac
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2020-08-19 08:22:08 -0500

    Updates

---
+++ precache-manifest.825960816a25f33187dda2016127c1ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1b556b95559439be0fc",
+    "url": "/app-badges/static/css/main.9a187562.chunk.css"
+  },
+  {
+    "revision": "d1b556b95559439be0fc",
+    "url": "/app-badges/static/js/main.d1b556b9.chunk.js"
+  },
+  {
+    "revision": "24f3627475d7cd24792f",
+    "url": "/app-badges/static/js/runtime~main.24f36274.js"
+  },
+  {
+    "revision": "0b8a42253817c88db74e",
+    "url": "/app-badges/static/css/2.f676c474.chunk.css"
+  },
+  {
+    "revision": "0b8a42253817c88db74e",
+    "url": "/app-badges/static/js/2.0b8a4225.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/app-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/app-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "d9c2e33e431f6e952326426e86b26039",
+    "url": "/app-badges/static/media/notfound.d9c2e33e.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/app-badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "997dd9b13277e367bcb16e5f9f55c7a1",
+    "url": "/app-badges/index.html"
+  }
+];
